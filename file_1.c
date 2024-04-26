5816852d71fb71821dce83172cb81315014d74ed
adityamp1 <2020wa86039@wilp.bits-pilani.ac.in>
2024-04-26 15:14:27 +0000

    added new function

--- file_1.c
+++ file_1.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
-int main() {    
+int main() {
 
     int number1, number2, sum;
-    
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
-    sum = number1 + number2;      
-    
+    sum = number1 + number2;
     printf("%d + %d = %d", number1, number2, sum);
     return 0;
+}
+
+//function added in branch1
+
+int func_sub(int a, int b)
+
+{
+
+diff = a-b;
+
+    return diff;
+
 }
